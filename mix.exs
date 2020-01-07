31d2ce6cd8c80c8d11614329c1cea00c5d4ea0ac
ryanyogan <ryanyogan@gmail.com>
2020-01-07 08:56:33 -0800

    added broadway, jason, httpoison deps

--- mix.exs
+++ mix.exs
@@ -11,7 +11,6 @@ defmodule WorldTemp.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +18,12 @@ defmodule WorldTemp.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.6"},
+      {:jason, "~> 1.1"},
+      {:broadway,
+       github: "plataformatec/broadway", tag: "08497708e10867935f2e92351e4cde9e4a57135e"}
     ]
   end
 end
